53bc9a62a5969d340bb29df7f5dce4443f8f773d
ernestchng <ernestchngyongcheng97@gmail.com>
2020-10-15 20:46:02 +0800

    Added basic pages and layout

---
+++ src/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :style="{backgroundImage: `url(${image})`}">
+    <div class="divider"></div>
+    <h1>ONE-STOP SOLUTION FOR COVID-19</h1>
+    <div class="divider"></div>
+  </div>
+</template>
+
+<script>
+import home from "@/assets/home2.jpg";
+
+export default {
+  data() {
+    return {
+      image: home
+    };
+  }
+};
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 800px;
+  color: #FFFFFF;
+  font-size: 20px;
+  text-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: inset 0 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.divider {
+  background: #FFFFFF;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  margin: 20px;
+  height: 5px;
+  width: 200px;
+}
+</style>
